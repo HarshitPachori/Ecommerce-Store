b81ebd5ddc5d50524bf699a561a07b18327e63d0
HarshitPachori <harshitpachori345@gmail.com>
2024-02-21 12:27:52 +0530

    added Inventory Service

---
+++ inventoryservice/src/test/java/com/example/inventoryservice/InventoryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
